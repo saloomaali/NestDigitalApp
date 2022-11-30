f450426e12cb7a48dd6214ed7bad4d63f15a03ee
Salooma <saloomaali2000@gmail.com>
2022-11-30 17:21:49 +0530

    change

--- src/app/employee-portal/employee-portal.component.ts
+++ src/app/employee-portal/employee-portal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-portal',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-portal.component.css']
 })
 export class EmployeePortalComponent {
+
+  EmpId = ""
+  password = ""
+
+  constructor(private route:Router){}
+
+  readValue=()=>{
+    let data:any = {"EmpId" : this.EmpId, "password" : this.password}
+    if(this.EmpId == "1122" && this.password == "1234"){
+      this.route.navigate(['/addCourse'])
+    }
+
+  }
 
 }
