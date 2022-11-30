6e60d643938d6c7c913f6f20b40c09ab65c39860
Salooma <saloomaali2000@gmail.com>
2022-11-30 14:40:58 +0530

    home page completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,59 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HompageComponent } from './hompage/hompage.component';
+import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
+import { EmployeePortalComponent } from './employee-portal/employee-portal.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+
+const myRouter:Routes = [
+  {
+    path:"",
+    component : HompageComponent
+  },
+  {
+    path:"about",
+    component: AboutComponent
+  },
+  {
+    path:"gallery",
+    component:GalleryComponent
+  },
+  {
+    path:"contact",
+    component:ContactComponent
+  },
+  {
+    path:"employeeLogin",
+    component:EmployeePortalComponent
+  },
+  {
+    path:"adminLogin",
+    component:AdminLoginComponent
+  }
+
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HompageComponent,
+    AboutComponent,
+    GalleryComponent,
+    ContactComponent,
+    EmployeePortalComponent,
+    AdminLoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRouter)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/hompage/hompage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hompage',
+  templateUrl: './hompage.component.html',
+  styleUrls: ['./hompage.component.css']
+})
+export class HompageComponent {
+
+}
